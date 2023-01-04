58cd313e546221869d165cecea57e2ff7e03cfb9
Adityo Arya Putra <adityo.arya.ft21@mail.umy.ac.id>
2023-01-04 15:46:50 +0700

    membuat class List dan Node Start

--- Exe5_040/Program.cs
+++ Exe5_040/Program.cs
@@ -12,6 +12,13 @@ namespace Exe5_040
         public string adityo;
         public Node arya;
     }
+
+    class List
+    {
+        Node START;
+        public List()
+        { }
+    }
     internal class Program
     {
         static void Main(string[] args)
